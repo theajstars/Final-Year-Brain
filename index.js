36110e158f0d3555177caed9ab6c8de825425ca4
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-05-02 16:08:34 +0100

    Should have pushed like 5 days ago when I created these models :-(

--- index.js
+++ index.js
@@ -1,48 +1,76 @@
-const getData = async () => {
-  await axios.get("Assets/JSON/df50.json").then((res) => {
-    console.log(res.data.data);
-  });
+console.clear();
+const baseURL = "http://127.0.0.1:8080/";
+
+const options = {
+  task: "classification",
+  debug: true,
 };
-getData();
-console.log(true);
-// const options = {
-//   dataUrl: "Assets/JSON/df.json",
-//   task: "classification",
-//   inputs: ["fever", "bodyPain", "age", "runnyNose", "diffBreath"],
-//   outputs: ["infectionProb"],
-//   debug: true,
-// };
+var correct = [];
+var wrong = [];
+const nn = ml5.neuralNetwork(options);
+nn.load("./TrainedModels/TopHalfTrained/model.json", () => {
+  console.log("Loaded model is readdy!");
+  //Fetch  data to test after
+  axios.get(`${baseURL}datasets/bottomhalf`).then((testingResult) => {
+    console.clear();
+    const testSegment = testingResult.data.file;
+    testSegment.map((testRecord) => {
+      const input = {
+        fever: testRecord.fever,
+        bodyPain: testRecord.bodyPain,
+        age: testRecord.age,
+        runnyNose: testRecord.runnyNose,
+        diffBreath: testRecord.diffBreath,
+      };
 
-// const nn = ml5.neuralNetwork(options, dataLoaded);
+      const output = testRecord.infectionProb.toString();
 
-// function dataLoaded() {
-//   const trainingOptions = {
-//     epochs: 32,
-//     batchSize: 12,
-//   };
+      function classify() {
+        nn.classify(input, (error, testResults) => {
+          if (error) {
+            console.error("Error: ", error);
+          } else {
+            const mapp = testResults.map((result, index) => {
+              if (result.label === output) {
+                if (result.confidence > 0.5) {
+                  //Prediction is correct
+                  const obj = {
+                    prediction: true,
+                    pos: testResults.indexOf(result),
+                  };
+                  // console.log("Prediction is correct!");
+                  correct.push(obj);
+                } else {
+                  //Prediction is inaccurate
+                  const obj = {
+                    prediction: false,
+                    pos: testResults.indexOf(result),
+                  };
+                  wrong.push(obj);
+                  // console.log("Prediction is inaccurate!");
+                }
+                console.log(
+                  `Label: ${result.label}, Confidence: ${result.confidence}`
+                );
+              }
 
-//   nn.train(trainingOptions, finishedTraining);
-// }
+              return {
+                length: testResults.length,
+                index,
+              };
+            });
 
-// function finishedTraining() {
-//   classify();
-// }
-
-// function classify() {
-//   const input = {
-//     fever: 102,
-//     bodyPain: 0,
-//     age: 97,
-//     runnyNose: 0,
-//     diffBreath: -1,
-//   };
-//   nn.classify(input, handleResults);
-// }
-
-// function handleResults(error, result) {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   console.log(result);
-// }
+            return Promise.all(mapp).then((e) => {
+              console.clear();
+              const wrongNum = wrong.length;
+              const correctNum = correct.length;
+              const total = wrongNum + correctNum;
+              console.log(Math.floor((correctNum / total) * 100));
+            });
+          }
+        });
+      }
+      classify();
+    });
+  });
+});
--- index2.js
+++ index2.js
@@ -1,118 +1,43 @@
-let data = [];
-async function getData() {
-  let data = await fetch("./Assets/JSON/df.json");
-  data = await data.json();
-  data = data.data;
-  data = data.slice(data.length / 10);
-  return data;
-}
+console.clear();
+const baseURL = "http://127.0.0.1:8080/";
 
-getData().then((res) => {
-  data = res;
-  const options = {
-    task: "classification",
-    debug: true,
-  };
-
-  const nn = ml5.neuralNetwork(options);
-  console.clear();
-  data.forEach((item) => {
-    const inputs = {
-      fever: item.fever,
-      bodyPain: item.bodyPain,
-      age: item.age,
-      runnyNose: item.runnyNose,
-      diffBreath: item.diffBreath,
-    };
-    const output = {
-      infectionProb: item.infectionProb.toString(),
+//Fetch data data to train
+axios
+  .get(`${baseURL}datasets/tophalf`)
+  .then((trainResultData) => {
+    const trainResult = trainResultData.data.file;
+    const options = {
+      task: "classification",
+      debug: true,
     };
 
-    nn.addData(inputs, output);
-  });
+    const nn = ml5.neuralNetwork(options);
+    trainResult.forEach((item) => {
+      const inputs = {
+        fever: item.fever,
+        bodyPain: item.bodyPain,
+        age: item.age,
+        runnyNose: item.runnyNose,
+        diffBreath: item.diffBreath,
+      };
+      const output = {
+        infectionProb: item.infectionProb.toString(),
+      };
 
-  nn.normalizeData();
-
-  const trainingOptions = {
-    epochs: 32,
-    batchSize: 12,
-  };
-
-  const values0 = [];
-  const values1 = [];
-  const input = {
-    fever: 102,
-    bodyPain: 0,
-    age: 97,
-    runnyNose: 0,
-    diffBreath: -1,
-  };
-  // nn.train(trainingOptions, finishedTraining);
-  // nn.train(trainingOptions, finishedTraining);
-  // nn.train(trainingOptions, finishedTraining);
-  nn.train(trainingOptions, () => {
-    nn.classify(input, (err, res) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        console.log(res);
-        res.map((result) => {
-          if (result.label === "0") {
-            //Case Negative test
-            values0.push(result.confidence);
-          } else {
-            //Case Positive Test
-            values1.push(result.confidence);
-          }
-        });
-      }
+      nn.addData(inputs, output);
     });
+
+    nn.normalizeData();
+
+    const trainingOptions = {
+      epochs: 32,
+      batchSize: 12,
+    };
+
     nn.train(trainingOptions, () => {
-      nn.classify(input, (err, res) => {
-        if (err) {
-          console.error(err);
-          return;
-        } else {
-          console.log(res);
-          res.map((result) => {
-            if (result.label === "0") {
-              //Case Negative test
-              values0.push(result.confidence);
-            } else {
-              //Case Positive Test
-              values1.push(result.confidence);
-            }
-          });
-        }
-      });
-
-      nn.train(trainingOptions, () => {
-        nn.classify(input, (err, res) => {
-          if (err) {
-            console.error(err);
-            return;
-          } else {
-            console.log(res);
-            res.map((result) => {
-              if (result.label === "0") {
-                //Case Negative test
-                values0.push(result.confidence);
-              } else {
-                //Case Positive Test
-                values1.push(result.confidence);
-              }
-            });
-            console.log(values0);
-            console.log(values1);
-            function reducer(index, accumulator) {
-              return index + accumulator;
-            }
-            console.log(values0.reduce(reducer));
-            console.log(values1.reduce(reducer));
-          }
-        });
-      });
+      nn.save();
     });
+  })
+  .catch((err) => {
+    console.error(err);
   });
-});
